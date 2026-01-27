93c316ae96bc472e05d3d0983585755ca7935914
luiscadari <luiscadari@icloud.com>
2026-01-27 06:55:42 -0300

    :sparkles: feat: user && reservation models

--- src/models/reservation.models.ts
+++ src/models/reservation.models.ts
@@ -1,6 +1,7 @@
 import { Column, Entity, ForeignKey, PrimaryGeneratedColumn } from 'typeorm';
 import { Session } from './session.models';
 import { Chair } from './chairs.models';
+import { User } from './user.models';
 
 @Entity()
 export class Reservation {
--- src/models/user.models.ts
+++ src/models/user.models.ts
@@ -1,7 +1,11 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
-
+@Entity()
 export class User {
-    id: number;
-    name: string;
-    cpf: string;
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column('string')
+  name: string;
+  @Column('string')
+  cpf: string;
 }
