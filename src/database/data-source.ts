11f5acaccd4093b6cb0fd5483fa412f6b107e185
luiscadari <luiscadari@icloud.com>
2026-01-28 10:30:44 -0300

    :sparkles: feat: data-source

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'root',
-  password: 'root',
-  database: 'cinema',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_DATABASE || 'cinema',
   synchronize: true,
   logging: true,
   entities: [],
